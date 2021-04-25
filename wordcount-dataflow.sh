2f6a3f9e52f04449bc6ac265038a0ddc21f5b870
joegagliardo <joey@me.com>
2021-04-25 20:40:47 +0000

    0.0.12

--- wordcount-dataflow.sh
+++ wordcount-dataflow.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
-
-PROJECT=dataflowclass1
-BUCKET=dataflowclass1-bucket
-REGION=us-east1
+PROJECT="qwiklabs-gcp-04-c21b49858f60"
+BUCKET="qwiklabs-gcp-04-c21b49858f60"
+REGION="us-central1"
 python3 -m apache_beam.examples.wordcount \
   --region $REGION \
   --input gs://dataflow-samples/shakespeare/kinglear.txt \
@@ -11,6 +10,6 @@ python3 -m apache_beam.examples.wordcount \
   --project $PROJECT \
   --temp_location gs://$BUCKET/tmp/
 
-  gsutil ls -lh "gs://$BUCKET/results/outputs*"  
-  gsutil cat "gs://$BUCKET/results/outputs*"
+gsutil ls -lh "gs://$BUCKET/results/outputs*"  
+gsutil cat "gs://$BUCKET/results/outputs*"
   

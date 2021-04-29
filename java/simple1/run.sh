b6db580616119871fbf1f367dbe86afc733be84f
joegagliardo <joey@me.com>
2021-04-29 03:35:28 +0000

    0.1.0

--- java/simple1/run.sh
+++ java/simple1/run.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
-rm -r src/main/java/com/mypackage/.ipynb_checkpoints
+rm -r src/main/java/com/mypackage/.ipynb_checkpoints 2> /dev/null
+rm regions_out* 2> /dev/null
 mvn compile exec:java -Dexec.mainClass="com.mypackage.Simple1"
 

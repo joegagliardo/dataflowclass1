01de5e9fa80ea7de1755cda32e295769141828ff
Joseph Gagliardo <joey@me.com>
2021-06-17 00:00:06 +0000

    0.2.3

--- wordcount-dataflow.sh
+++ wordcount-dataflow.sh
@@ -1,7 +1,4 @@
 #! /bin/sh
-PROJECT="qwiklabs-gcp-04-c21b49858f60"
-BUCKET="qwiklabs-gcp-04-c21b49858f60"
-REGION="us-central1"
 python3 -m apache_beam.examples.wordcount \
   --region $REGION \
   --input gs://dataflow-samples/shakespeare/kinglear.txt \

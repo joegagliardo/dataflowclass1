74e7f65ae17b145e7a43d0506c24baea00a33a16
joegagliardo <joey@me.com>
2021-09-21 08:42:21 +0000

    0.2.5

--- dataflow-sql-notes.sh
+++ dataflow-sql-notes.sh
@@ -22,6 +22,15 @@ gcloud pubsub subscriptions create my_subscription --topic my_topic && gcloud pu
     }
 ]
 
+
+gcloud components update
+
+sudo apt-get update && sudo apt-get --only-upgrade install google-cloud-sdk-config-connector google-cloud-sdk-kubectl-oidc google-cloud-sdk google-cloud-sdk-skaffold kubectl google-cloud-sdk-app-engine-grpc google-cloud-sdk-app-engine-java google-cloud-sdk-firestore-emulator google-cloud-sdk-bigtable-emulator google-cloud-sdk-minikube google-cloud-sdk-anthos-auth google-cloud-sdk-pubsub-emulator google-cloud-sdk-datastore-emulator google-cloud-sdk-app-engine-go google-cloud-sdk-spanner-emulator google-cloud-sdk-local-extract google-cloud-sdk-app-engine-python google-cloud-sdk-datalab google-cloud-sdk-cbt google-cloud-sdk-kpt google-cloud-sdk-gke-gcloud-auth-plugin google-cloud-sdk-app-engine-python-extras google-cloud-sdk-cloud-build-local
+
+gcloud data-catalog entries update \
+--lookup-entry='pubsub.topic.`qwiklabs-gcp-02-d0ca2f823b47`.my_topic' \
+--schema-from-file=my_topic_schema.yaml
+
 SELECT user_id, COUNT(http_request) as request_cnt, TUMBLE_START('INTERVAL 2 MINUTE') as window_start
 FROM pubsub.topic.`qwiklabs-gcp-02-70a385c1381f`.`my_topic`
 GROUP BY user_id, TUMBLE(event_timestamp, 'INTERVAL 2 MINUTE')

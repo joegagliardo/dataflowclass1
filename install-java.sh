250d7d11bdd940ab8ca8aba97adaee8fc258abbf
joegagliardo <joey@me.com>
2022-01-28 20:37:18 +0000

    0.3.23

--- install-java.sh
+++ install-java.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y default-jdk
-cd /tmp
+apt-get install -y nano default-jdk
 wget -q -nc -O gradle.zip https://services.gradle.org/distributions/gradle-5.0-bin.zip
-unzip -q -d /opt gradle.zip'
-rm gradle-5.0-bin.zip
+rm -rf /opt/gradle*
+unzip -q -d /opt gradle.zip
+rm gradle.zip
 
